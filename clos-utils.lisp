85968003ce3ad158a801ba1c2ce1726564c210c3
Blake McBride <blake@arahant.com>
2018-04-01 21:14:49 -0500

    Initial commit

---
+++ clos-utils.lisp
@@ -0,0 +1,48 @@
+
+;  Common Lisp CLOS utilities
+;
+;  Written by:
+;        Blake McBride
+;        blake@mcbride.name
+;        http://blake.mcbride.name
+;
+;  Version / Release 8/6/2008
+;
+;  Donated to the public domain.
+
+
+(defmacro set-slot (i s v)
+  `(setf (slot-value ,i ',s) ,v))
+
+(defmacro get-slot (i s)
+  `(slot-value ,i ',s))
+
+(defclass metaclass (standard-class) ())
+
+#+sbcl           (defmethod sb-mop:validate-superclass ((class metaclass) (superclass standard-class)) t)
+#+(or cmu gcl)   (defmethod pcl:validate-superclass ((class metaclass) (superclass standard-class)) t)
+
+(defmacro defclass2 (class-name super-class-list class-variables instance-variables)
+  (flet ((add-meta (sym)
+		   (intern (concatenate 'string "META-" (symbol-name sym))))
+	 (make-global (x)
+		      (if (consp x)
+			  (append x '(:allocation :class))
+			(cons x '(:allocation :class)))))
+	`(progn
+	   (defclass ,(add-meta class-name)
+	     ,(if (null super-class-list)
+		  '(metaclass)
+		(mapcar #'add-meta super-class-list))
+	     ,(mapcar #'make-global class-variables)
+	     (:metaclass metaclass))
+	   (defclass ,class-name
+	     ,(if (null super-class-list)
+		  '(standard-object)
+		super-class-list)
+	     ,instance-variables
+	     (:metaclass ,(add-meta class-name)))
+	   (defvar ,class-name (find-class ',class-name))
+;          (defvar ,(add-meta class-name) (find-class ',(add-meta class-name)))
+	   )))
+
