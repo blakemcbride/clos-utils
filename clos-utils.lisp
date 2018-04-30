191967e277dbd4748ad0264291e9c97195634ee2
Blake McBride <blake@arahant.com>
2018-04-30 18:16:42 -0500

    Added defmethod2

--- clos-utils.lisp
+++ clos-utils.lisp
@@ -6,9 +6,10 @@
 ;        blake@mcbride.name
 ;        http://blake.mcbride.name
 ;
-;  Version / Release 4/1/2018
+;  Version / Release 4/30/2018
 ;
 ;  Donated to the public domain.
+
 
 
 (defmacro set-slot (i s v)
@@ -25,6 +26,7 @@
 #+ccl            (defmethod ccl:validate-superclass ((class metaclass) (superclass standard-class)) t)
 
 (defmacro defclass2 (class-name super-class-list class-variables instance-variables)
+  "defclass2 defines a CLOS class with a parallel meta-class structure ala Smalltalk."
   (flet ((add-meta (sym)
 		   (intern (concatenate 'string "META-" (symbol-name sym))))
 	 (make-global (x)
@@ -47,4 +49,13 @@
 	   (defvar ,class-name (find-class ',class-name))
 ;          (defvar ,(add-meta class-name) (find-class ',(add-meta class-name)))
 	   )))
+
+(defmacro defmethod2 (method-name class-name arg-list &rest body)
+  "defmethod2 defines a fixed argument method and associates it to a variable argument generic.
+   This allows the same method name in different classes to have a different number of fixed arguments."
+  (let ((alist (gensym)))
+    `(defmethod ,method-name ((self ,class-name) &rest ,alist)
+       (apply #'(lambda (self ,@arg-list) ,@body)
+	      (cons self ,alist)))))
+
 
